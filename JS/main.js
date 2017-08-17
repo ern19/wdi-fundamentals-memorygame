370ae69329680cecac089f0320483108575fcd4f
Aaron Trammell <aarontrammell@Aarons-MacBook-Pro.local>
2017-08-17 13:37:13 -0400

    finished memory game

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,32 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/images/queen-of-diamonds.png"
+	},
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/images/queen-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/images/king-of-diamonds.png"
+	}
+
+];
+
+
 var cardsInPlay = [];
 
-var checkForMatch = function(){
+var checkForMatch = function() {
+
  if(cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("you found a match");
@@ -12,14 +37,28 @@ var checkForMatch = function(){
  	};
 };
 
-var flipCard = function(cardId) {
-	console.log("User flipped" + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
+
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+			cardElement.setAttribute('src', 'images/images/back.png');
+			cardElement.setAttribute('data-id', i);
+			cardElement.addEventListener('click', flipCard);
+			document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+createBoard();
 
 
 
